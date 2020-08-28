716b6238537ce547c0600322f08c4b5cb53d47bb
bhawnishk1 <mrbhawnish@gmail.com>
2020-08-28 16:29:01 -0700

    Stretch completed

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import data from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+// Contexts
 import ProductContext from "./contexts/ProductContexts";
 import CartContext from "./contexts/CartContext";
-
 
 function App() {
   const [products] = useState(data);
@@ -19,23 +19,29 @@ function App() {
     setCart([...cart, item]);
   };
 
+  const removeItem = (id) => {
+
+	const newCart = cart.filter(item => item.id !== id)
+	  return setCart(newCart)
+  }
+
   return (
     <div className="App">
       {/* Provide the Data with our custom Product Context */}
       <ProductContext.Provider value={{ products, addItem }}>
-	  <CartContext.Provider value={{cart}}>
-        <Navigation />
+        <CartContext.Provider value={{ cart, removeItem }}>
+          <Navigation />
 
-        {/* Routes */}
+          {/* Routes */}
 
-        <Route exact path="/">
-          <Products />
-        </Route>
+          <Route exact path="/">
+            <Products />
+          </Route>
 
-        <Route path="/cart">
-          <ShoppingCart />
-        </Route>
-		</CartContext.Provider>
+          <Route path="/cart">
+            <ShoppingCart />
+          </Route>
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
